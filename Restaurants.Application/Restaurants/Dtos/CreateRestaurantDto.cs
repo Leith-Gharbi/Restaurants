becdc263c577c0889124cffbbc543cbb34d20107
Gharbi <leithgharbi@yahoo.fr>
2024-08-11 15:13:00 +0200

    33. Modal validation

--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +9,29 @@ namespace Restaurants.Application.Restaurants.Dtos
 {
     public class CreateRestaurantDto
     {
-        public int Id { get; set; }
+
+
+
+        [StringLength(100,MinimumLength =3 )]
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
 
+        [Required(ErrorMessage ="Insert a valid category")]
         public string Category { get; set; } = default!;
+        public bool HasDelivery { get; set; } = default!;
 
+
+        [EmailAddress(ErrorMessage ="Please provide a valid email address")]
         public string? ContactEmail { get; set; }
+
+        [Phone(ErrorMessage = "Please provide a valid phone number")]
         public string? ContactNumber { get; set; }
 
 
         public string? City { get; set; }
         public string? Street { get; set; }
+
+        [RegularExpression(@"^\d{2}-\d{3}$" ,ErrorMessage = "Please provide a valid postal code  (XX-XXX)")]
         public string? PostalCode { get; set; }
     }
 }
