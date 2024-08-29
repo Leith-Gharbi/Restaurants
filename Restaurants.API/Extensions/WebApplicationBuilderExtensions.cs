8b10b4d780e41e62607f2705e8cc08012dcff014
Gharbi <leithgharbi@yahoo.fr>
2024-08-29 17:23:47 +0200

    58. Extracting presentation layer services

---
+++ Restaurants.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.OpenApi.Models;
+using Restaurants.API.Middlewares;
+using Serilog;
+
+namespace Restaurants.API.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static void AddPresentation(this WebApplicationBuilder builder )
+        {
+
+            builder.Services.AddControllers();
+            // Add Swagger Service
+            builder.Services.AddSwaggerGen(c =>
+            {
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "My API",
+                    Description = "My API Description",
+                });
+
+                c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                });
+
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type =ReferenceType.SecurityScheme,
+                    Id= "bearerAuth"
+                }
+            },[]
+        }
+
+    });
+            });
+
+
+            //Register the  Error Handling middleware  as dependency 
+            builder.Services.AddScoped<ErrorHandlingMiddleware>();
+            //Register the  Request TimeLogging middleware  as dependency 
+            builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+
+            // Add Serilog 
+            builder.Host.UseSerilog((context, configuration) =>
+             configuration
+             .ReadFrom.Configuration(context.Configuration)  // this to indicate that serilog parameters are from appseting.json 
+
+             // this when you config serilog in program.cs
+             //.MinimumLevel.Override("Microsoft",LogEventLevel.Warning)
+             //.MinimumLevel.Override("Microsoft.EntityFrameworkCore",LogEventLevel.Information)
+             //.WriteTo.File("Logs/Restaurant-API-.log",rollingInterval:RollingInterval.Day,rollOnFileSizeLimit: true) // create a  Log folder and generate a log file every day 
+             //.WriteTo.Console(outputTemplate : "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
+             );
+
+        }
+
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,79 +1,25 @@
 
-using Microsoft.OpenApi.Models;
+
+using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
-using Serilog.Events;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// Add Swagger Service
-builder.Services.AddSwaggerGen(c =>
-{
-
-
-
-
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "My API",
-        Description = "My API Description",
-    });
-
-
-
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-    });
-
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type =ReferenceType.SecurityScheme,
-                    Id= "bearerAuth"
-                }
-            },[]
-        }
-
-    });
-});
-
-
-//Register the  Error Handling middleware  as dependency 
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-//Register the  Request TimeLogging middleware  as dependency 
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+builder.AddPresentation();
 
 builder.Services.AddApplication();
-
-// Add Serilog 
-builder.Host.UseSerilog((context, configuration) => 
- configuration
- .ReadFrom.Configuration(context.Configuration)  // this to indicate that serilog parameters are from appseting.json 
-
- // this when you config serilog in program.cs
-        //.MinimumLevel.Override("Microsoft",LogEventLevel.Warning)
-        //.MinimumLevel.Override("Microsoft.EntityFrameworkCore",LogEventLevel.Information)
-        //.WriteTo.File("Logs/Restaurant-API-.log",rollingInterval:RollingInterval.Day,rollOnFileSizeLimit: true) // create a  Log folder and generate a log file every day 
-        //.WriteTo.Console(outputTemplate : "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
- );
 
 var app = builder.Build();
 
